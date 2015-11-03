c1db1c84deefd1a336c29c5043733af3af0f1843
claudiob <claudiob@gmail.com>
2015-11-03 10:09:43 -0800

    Use implicit 'document' by Prawn

--- lib/elegant/document.rb
+++ lib/elegant/document.rb
@@ -12,11 +12,11 @@ module Elegant
     # Initializes a Document. Some options are fixed (e.g. the size is 612x792),
     # others can be customized through +options+.
     def initialize(options = {})
-      super(page_settings info: options[:metadata]) do |document|
-        set_fonts document
-        render_header document, options.fetch(:header, {})
-        yield document if block_given?
-        render_footer document, options.fetch(:footer, {})
+      super(page_settings info: options[:metadata]) do
+        set_fonts
+        render_header options.fetch(:header, {})
+        yield if block_given?
+        render_footer options.fetch(:footer, {})
       end
     end
 
@@ -65,51 +65,51 @@ module Elegant
     # Set the fonts for the document. Fonts are provided via configuration and
     # a 'Fallback' font must be set to be used for special characters. A
     # 'Sans Serif' font is also required for titles and headers.
-    def set_fonts(document)
+    def set_fonts
       fonts = Elegant.configuration.fonts.transform_keys{|k| k.to_s.titleize}
-      document.font_families.update fonts
-      document.fallback_fonts ['Fallback']
+      font_families.update fonts
+      fallback_fonts ['Fallback']
     end
 
     # Draws in the header of each page a watermark logo (provided via the
     # configuration), a horizontal line, a title and an image for the content.
-    def render_header(document, options = {})
-      document.repeat(:all) do
-        render_watermark document
-        document.stroke_horizontal_rule
-        render_logo document, options[:logo]
-        render_heading document, options[:text]
+    def render_header(options = {})
+      repeat(:all) do
+        render_watermark
+        stroke_horizontal_rule
+        render_logo options[:logo]
+        render_heading options[:text]
       end
     end
 
     # Renders a watermark at the top-left corner of each page. The watermark
     # must be provided via configuration.
-    def render_watermark(document)
+    def render_watermark
       image = Elegant.configuration.watermark
       height = (header_height * 0.25).ceil
-      y = document.bounds.top + (header_height * 0.375).floor
-      document.image image, at: [0, y], height: height.ceil
+      y = bounds.top + (header_height * 0.375).floor
+      image image, at: [0, y], height: height.ceil
     end
 
     # Renders an image in the top-right corner of each page. The image must be
     # provided when initializing the document.
-    def render_logo(document, logo)
+    def render_logo(logo)
       return unless logo
       url = logo[:url]
       @logo_width = w = logo.fetch :width, 50
       h = logo.fetch :height, 50
 
-      lw = document.line_width
-      left = document.bounds.right - w - 1.5 * lw
-      top = document.bounds.top + h / 2 + 0.5 * lw
-      position = [left + 0.5 * lw, document.bounds.top + h / 2]
+      lw = line_width
+      left = bounds.right - w - 1.5 * lw
+      top = bounds.top + h / 2 + 0.5 * lw
+      position = [left + 0.5 * lw, bounds.top + h / 2]
 
-      document.float do
-        document.bounding_box [left, top], width: w + lw, height: h + lw do
-          document.stroke_bounds
+      float do
+        bounding_box [left, top], width: w + lw, height: h + lw do
+          stroke_bounds
         end
         begin
-          document.image open(logo[:url]), width: w, height: h, at: position
+          image open(logo[:url]), width: w, height: h, at: position
         rescue OpenURI::HTTPError, OpenSSL::SSL::SSLError, SocketError
         end
       end
@@ -118,59 +118,59 @@ module Elegant
     # Writes the heading for the document in the top-right corner of each page,
     # to the left of the logo. The heading must be provided when initializing
     # the document.
-    def render_heading(document, text)
+    def render_heading(text)
       return unless text
-      document.transparent(0.25) do
-        document.font('Sans Serif', style: :bold) do
-          document.text_box text, heading_options(document)
+      transparent(0.25) do
+        font('Sans Serif', style: :bold) do
+          text_box text, heading_options
         end
       end
     end
-    
-    def heading_options(document)
+
+    def heading_options
       left = 25
-      width = logo_width + 2 * document.line_width + (margin = 5)
+      width = logo_width + 2 * line_width + (margin = 5)
       {}.tap do |options|
         options[:valign] = :center
         options[:align] = :right
         options[:overflow] = :shrink_to_fit
-        options[:width] = document.bounds.width - width - left
+        options[:width] = bounds.width - width - left
         options[:height] = header_height / 2.0
         options[:size] = 17
-        options[:at] = [left, document.bounds.top + options[:height]]
+        options[:at] = [left, bounds.top + options[:height]]
       end
     end
 
     # Draws in the header of each page a horizontal line, the name of the
     # author, the title and the page number. The author must be provided in
     # the configuration, and the title when initializing the document.
-    def render_footer(document, footer)
+    def render_footer(footer)
       text = "<link href='#{footer[:url]}'>#{footer[:text]}</link>"
       left = 50
       options = {size: 7, align: :center, valign: :top, inline_format: true}
       options[:at] = [left, -6]
-      options[:width] = document.bounds.width - 2 * left
+      options[:width] = bounds.width - 2 * left
       options[:height] = 10
 
-      document.repeat(:all) do
-        document.transparent(0.25) do
-          document.stroke_horizontal_line 0, document.bounds.width, at: 0
+      repeat(:all) do
+        transparent(0.25) do
+          stroke_horizontal_line 0, bounds.width, at: 0
         end
-        render_author document
-        document.text_box text, options
+        render_author
+        text_box text, options
       end
-      render_page_number document
+      render_page_number
     end
 
-    def render_author(document)
+    def render_author
       options = {at: [0, -6], width: 50, height: 10, size: 7, valign: :top}
-      document.text_box Elegant.configuration.author, options
+      text_box Elegant.configuration.author, options
     end
 
-    def render_page_number(document)
+    def render_page_number
       options = {width: 50, height: 10, size: 7, align: :right, valign: :top}
-      options[:at] = [document.bounds.width - options[:width], -6]
-      document.number_pages "Page <page>", options
+      options[:at] = [bounds.width - options[:width], -6]
+      number_pages "Page <page>", options
     end
   end
 end
